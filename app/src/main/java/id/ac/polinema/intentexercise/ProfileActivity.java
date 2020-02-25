1d9d3a950fb6f69376fbd3bdabf8b8150f451ea8
DelamontinWP <delamontin.wp@gmail.com>
2020-02-25 07:11:34 +0700

    add validasi data

--- app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
@@ -2,13 +2,55 @@ package id.ac.polinema.intentexercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static id.ac.polinema.intentexercise.RegisterActivity.ABOUT_KEY;
+import static id.ac.polinema.intentexercise.RegisterActivity.EMAIL_KEY;
+import static id.ac.polinema.intentexercise.RegisterActivity.FULLNAME_KEY;
+import static id.ac.polinema.intentexercise.RegisterActivity.HOMEPAGE_KEY;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private TextView fullname;
+    private TextView email;
+    private TextView homepage;
+    private TextView about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        about =findViewById(R.id.label_about);
+        fullname =findViewById(R.id.label_fullname);
+        email =findViewById(R.id.label_email);
+        homepage =findViewById(R.id.label_homepage);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            String fullnameText = bundle.getString("FULLNAME_KEY");
+            String emailText = bundle.getString("EMAIL_KEY");
+            String homepageText = bundle.getString("HOMEPAGE_KEY");
+            String aboutText = bundle.getString("ABOUT_KEY");
+
+            fullname.setText(fullnameText);
+            email.setText(emailText);
+            homepage.setText(homepageText);
+            about.setText(aboutText);
+        }
+    }
+
+    public void homepage(View view) {
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null){
+            String homepageText = bundle.getString("HOMEPAGE_KEY");
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://"+homepageText));
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
@@ -2,13 +2,63 @@ package id.ac.polinema.intentexercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    public static final String FULLNAME_KEY = "fullname";
+    public static final String EMAIL_KEY = "email";
+    public static final String PASSWORD_KEY = "password";
+    public static final String CONFIRM_KEY = "confirm";
+    public static final String HOMEPAGE_KEY = "homepage";
+    public static final String ABOUT_KEY = "about";
+
+    private EditText fullnameInput;
+    private EditText emailInput;
+    private EditText passwordInput;
+    private EditText confirmInput;
+    private EditText homepageInput;
+    private EditText aboutInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        fullnameInput = findViewById(R.id.text_fullname);
+        emailInput = findViewById(R.id.text_email);
+        passwordInput = findViewById(R.id.text_password);
+        confirmInput = findViewById(R.id.text_confirm_password);
+        homepageInput = findViewById(R.id.text_homepage);
+        aboutInput = findViewById(R.id.text_about);
+    }
+
+    public void handleProfile(View view) {
+        String fullname = fullnameInput.getText().toString();
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        String confirm = confirmInput.getText().toString();
+        String homepage = homepageInput.getText().toString();
+        String about = aboutInput.getText().toString();
+
+        if(!(fullname).equals("") && !(email).equals("") && !(password).equals("") && !(confirm).equals("") && !(homepage).equals("") && !(about).equals("")){
+            if((password).equals(confirm)){
+                Intent intent = new Intent(this,ProfileActivity.class);
+                intent.putExtra("FULLNAME_KEY", fullname);
+                intent.putExtra("EMAIL_KEY", email);
+                intent.putExtra("PASSWORD_KEY", password);
+                intent.putExtra("CONFIRM_KEY", confirm);
+                intent.putExtra("HOMEPAGE_KEY", homepage);
+                intent.putExtra("ABOUT_KEY", about);
+                startActivity(intent);
+            }else{
+                Toast.makeText(this, "Password dan konfirmasi Password harus sama",Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(this, "Data harus terisi semua",Toast.LENGTH_SHORT).show();
+        }
     }
 }
